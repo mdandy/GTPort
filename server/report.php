0bb95894ede729060a63eb5b814ba44167c8b298
Michael Dandy <mdandy1988@gmail.com>
2012-12-02 00:44:59 -0500

    Report API part deux

---
+++ server/report.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once("dal.php");
+require_once("json.php");
+
+if (strcmp($_SERVER['REQUEST_METHOD'], 'GET') == 0)
+{
+	$q = $_POST["q"];
+	
+	$ret = NULL;
+	if (strcmp($q, "admin") == 0)
+	{
+		DAL::connect();
+		$report = DAL::get_admin_report();
+		DAL::disconnect();
+		
+		if ($report != NULL)
+			$ret = array ("res" => "TRUE", "data" => $report);
+		else
+			$ret = array ("res" => "FALSE");
+	}
+	else if (strcmp($q, "faculty") == 0)
+	{
+		DAL::connect();
+		$report = DAL::get_faculty_report();
+		DAL::disconnect();
+		
+		if ($report != NULL)
+			$ret = array ("res" => "TRUE", "data" => $report);
+		else
+			$ret = array ("res" => "FALSE");
+	}
+	else if (strcmp($q, "student") == 0)
+	{
+		DAL::connect();
+		$report = DAL::get_student_report();
+		DAL::disconnect();
+		
+		if ($report != NULL)
+			$ret = array ("res" => "TRUE", "data" => $report);
+		else
+			$ret = array ("res" => "FALSE");
+	}
+	
+	echo (json_encode($ret));
+}
+
+?>
